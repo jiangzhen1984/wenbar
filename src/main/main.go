921fcd93c17ca06b2659abfb6671a4502c68e378
Jiang Zhen <jiangzhen1984@126.com>
2016-10-01 17:46:41 +0800

    Fix bug use file for initalize wechat config

--- src/main/main.go
+++ src/main/main.go
@@ -11,8 +11,12 @@ import (
 
 
 func main() {
-    wechat.WeChatConfs = []*wechat.WeChatConfig{WechatConf}
-    WechatConf.AuthServer()
+    wechat.WeChatConfs = wechat.InitWeChatConfig("conf/conf.json")
+    if wechat.WeChatConfs == nil || len(wechat.WeChatConfs) <= 0{
+           gotom.LP("Initalize wechat config failed \n")
+           return
+    }
+    wechat.DC().AuthServer()
     gotom.LI("[wechat] auth initalized ==>%s\n", WechatConf)
     ws.InitDB(ws.DBConfiguration{DBUrl:"localhost"})
     gotom.InitServer(conf)
