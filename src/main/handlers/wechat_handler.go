8908751b6964c8609813791a6e1456d29e1924f3
Jiang Hunk <hunk.jiang@sonymobile.com>
2016-10-06 12:38:24 +0800

    Add wechat response hanlder

--- src/main/handlers/wechat_handler.go
+++ src/main/handlers/wechat_handler.go
@@ -5,7 +5,78 @@ import (
     "gotom"
     "main/service/wechat"
     "main/service/vo"
+    "main/service"
+    "time"
 )
+
+type weChatRespHdr struct {
+     
+     user    * vo.User
+     wechat  * wechat.WeChatUser
+}
+
+
+func (h * weChatRespHdr) OnResponse(t int, us * wechat.WeChatUser, ret bool, data interface{}) {
+     if !ret {
+           gotom.LD(" Handle wechatuser auth failed \n")
+           return
+     }
+
+     switch t {
+          case wechat.RESPONSE_TYPE_USER_AUTH:
+                h.getUserTokenInfo(us, ret, data)
+          case wechat.RESPONSE_TYPE_GET_USER_INFO:
+                h.getUserInfo(us, ret, data)
+          default:
+                gotom.LW("Unkown type of response %d\n", t)
+     }
+
+}
+
+
+func (h * weChatRespHdr) getUserTokenInfo(user * wechat.WeChatUser, ret bool, data interface{}) {
+     var pwc *vo.PersonalWeChat
+     ar := data.(*wechat.AuthResponse)
+     if h.user.WeChat == nil {
+         pwc = new(vo.PersonalWeChat)
+         h.user.WeChat = pwc
+     } else {
+         pwc = h.user.WeChat 
+     }
+     pwc.OpenId        = ar.Openid
+     pwc.Token         = ar.Access_token
+     pwc.TokenTime     = time.Now().Unix()
+     pwc.TokenExpired  = ar.Expires_in
+    
+     gotom.LI("====> got token user %s\n", ar)
+     if _, err := ws.DoService(ws.UpdateUserWeChat, h.user); err != nil {
+          gotom.LE("update wechat token failed %s\n", err)
+     }
+}
+
+func (h * weChatRespHdr) getUserInfo(user * wechat.WeChatUser, ret bool, data interface{}) {
+     var pwc *vo.PersonalWeChat
+     ar := data.(*wechat.UserInfoResp)
+     if h.user.WeChat == nil {
+         pwc = new(vo.PersonalWeChat)
+         h.user.WeChat = pwc
+     } else {
+         pwc = h.user.WeChat 
+     }
+     pwc.NickName    = ar.NickName
+     pwc.City        = ar.City
+     pwc.Unionid     = ar.UnionId
+     pwc.Avatar      = ar.Headimgurl
+     pwc.Sex         = ar.Sex
+     pwc.Country     = ar.Country
+     h.user.Avatar1  = pwc.Avatar
+
+     gotom.LI("====> get user info user %s\n", ar)
+     if _, err := ws.DoService(ws.UpdateUserWeChat, h.user); err != nil {
+          gotom.LE("update wechat information failed %s\n", err)
+     }
+}
+
 
 func WeChatHandler(resp gotom.GTResponse, req * gotom.GTRequest, tpls * gotom.GTTemplateMapping)  (*gotom.GTTemplate, gotom.Object, error) {
 
@@ -20,7 +91,7 @@ func WeChatHandler(resp gotom.GTResponse, req * gotom.GTRequest, tpls * gotom.GT
 
          wechatuser := wechat.DC().InitWeChatUser()
          wechatuser.UpdateAuthCode(code)
-         wechatuser.AuthToken(user)
+         wechatuser.AuthToken(wechat.WeChatRespHandler(&weChatRespHdr{user : user, wechat : wechatuser}))
          sess.SetAttribute("wechatuser", wechatuser)
          //TODO get user info
          Redirect(resp, req, "/hot_list")
--- src/main/service/vo/user.go
+++ src/main/service/vo/user.go
@@ -3,9 +3,6 @@ package vo
 
 import (
     "strconv"
-    "gotom"
-    "time"
-    "main/service/wechat"
 )
 
 
@@ -79,69 +76,4 @@ type PersonalWeChat struct {
 }
 
 
-
-func (u User) OnResponse(t int, us * wechat.WeChatUser, ret bool, data interface{}) {
-     if !ret {
-           gotom.LD(" Handle wechatuser auth failed \n")
-           return
-     }
-
-     switch t {
-          case wechat.RESPONSE_TYPE_USER_AUTH:
-                u.getUserTokenInfo(us, ret, data)
-          case wechat.RESPONSE_TYPE_GET_USER_INFO:
-                u.getUserInfo(us, ret, data)
-          default:
-                gotom.LW("Unkown type of response %d\n", t)
-     }
-
-}
-
-
-func (u User) getUserTokenInfo(user * wechat.WeChatUser, ret bool, data interface{}) {
-     var pwc *PersonalWeChat
-     ar := data.(*wechat.AuthResponse)
-     if u.WeChat == nil {
-         pwc = new(PersonalWeChat)
-         u.WeChat = pwc
-     } else {
-         pwc = u.WeChat 
-     }
-     pwc.OpenId        = ar.Openid
-     pwc.Token         = ar.Access_token
-     pwc.TokenTime     = time.Now().Unix()
-     pwc.TokenExpired  = ar.Expires_in
-    
-     gotom.LI("====> got token user %s\n", ar)
- /*    gobj := gotom.Object(&u)
-     if _, err := ws.DoService(ws.UpdateUserWeChat, &gobj); err != nil {
-          gotom.LE("update wechat token failed %s\n", err)
-     }
-*/
-}
-
-func (u User) getUserInfo(user * wechat.WeChatUser, ret bool, data interface{}) {
-     var pwc *PersonalWeChat
-     ar := data.(*wechat.UserInfoResp)
-     if u.WeChat == nil {
-         pwc = new(PersonalWeChat)
-         u.WeChat = pwc
-     } else {
-         pwc = u.WeChat 
-     }
-     pwc.NickName    = ar.NickName
-     pwc.City        = ar.City
-     pwc.Unionid     = ar.UnionId
-     pwc.Avatar      = ar.Headimgurl
-     pwc.Sex         = ar.Sex
-     pwc.Country     = ar.Country
-     u.Avatar1       = pwc.Avatar
-
-     gotom.LI("====> get user info user %s\n", ar)
-/*     gobj := gotom.Object(&u)
-     if _, err := ws.DoService(ws.UpdateUserWeChat, &gobj); err != nil {
-          gotom.LE("update wechat information failed %s\n", err)
-     }
-*/
-}
 
