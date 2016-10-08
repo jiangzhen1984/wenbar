eb2e7acee850a74c30616641355ecc6db73cf021
Jiang Hunk <hunk.jiang@sonymobile.com>
2016-10-08 15:12:50 +0800

    Add start argument

--- src/main/main.go
+++ src/main/main.go
@@ -6,14 +6,45 @@ import (
     "gotom"
     "main/service"
     "main/service/wechat"
- //   "simulation"
+    "simulation"
+    "os"
 )
 
 
 
 func main() {
-//    go sim.InitSIMWeChatServer()
-    wechat.WeChatConfs = wechat.InitWeChatConfig("conf/conf.json")
+    
+    var simuflag bool = false
+    var wechatConfigPath string
+    var port string
+    var debugmode bool = false
+    for idx, arg := range os.Args {
+         switch {
+            case arg == "-s":
+                simuflag = true
+            case arg == "-w":
+                if len(os.Args) - 1 <= idx {
+                     gotom.LP(" argument incorrect -w filepath ")
+                }
+                wechatConfigPath = os.Args[idx + 1]
+            case arg == "-p":
+                if len(os.Args) - 1 <= idx {
+                     gotom.LP(" argument incorrect -p port ")
+                }
+                port = os.Args[idx + 1]
+            case arg == "-d":
+                debugmode = true
+                
+         }
+    }
+
+    conf.Port = ":" + port
+    conf.DebugMode = debugmode
+    if simuflag == true {
+         gotom.LI("Use local wechat simuation server")
+         go sim.InitSIMWeChatServer()
+    }
+    wechat.WeChatConfs = wechat.InitWeChatConfig(wechatConfigPath)
     if wechat.WeChatConfs == nil || len(wechat.WeChatConfs) <= 0{
            gotom.LP("Initalize wechat config failed \n")
            return
